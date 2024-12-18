88712b4a4c3c80f5d6ae0182140e6cfda040c2d5
Shefat Ahmed <shefata69@gmail.com>
2024-12-18 19:07:08 +0600

    complete practice Variables & Data Types

--- Variables & Data Types/first.js
+++ Variables & Data Types/first.js
@@ -1,25 +1,54 @@
-// console.log("Hello World!");
-// console.log("Welcome , Shefat Ahmed");
-// console.log("How are you!");
+// Our 1st JS Code
+console.log("Hello World!");
+console.log("Welcome , Shefat Ahmed");
+console.log("How are you!");
 
-// fullName = "Shefat Ahmed"
-// age=24
-// price=101
-// radius=44.55
-// a=null
-// y=undefined
-// isFollow=true //or false
-// console.log(isFollow)
+// vaiable in javascript
+fullName = "Shefat Ahmed"
+age=24
+price=101
+radius=44.55
+a=null
+y=undefined
+isFollow=true //or false
+console.log(isFollow)
 
-// fullName= "Kandapara"
-// fullname= "Malshapara"
+// variable rules
+fullName= "Kandapara"
+fullname= "Malshapara"
+fullName = "tony stark";
+_fullName = "tony stark";
+$fullName = "tony stark";
+fullname ="tony stark"
+console= "tony stark" //reserve keyword
 
-// console.log(fullName)
+// let const var
+var age = 24;
+var age = 35;
+var age = 15;
+let age = 23;
+age = 24;
+const age = 23;
+console.log(age)
 
-// fullName = "tony stark";
-// _fullName = "tony stark";
-// $fullName = "tony stark";
-// fullname ="tony stark"
-
-// console= "tony stark" //reserve keyword
-// console.log(console);
+// DataTypes in Js
+// primitive data types(7)
+let age = 24;
+let name = "shefat";
+let isFollow = true;
+let Xundefined;
+let x = null;
+let xBigint = BigInt("123");
+let xSymbol = Symbol("Shefat");
+console.log(typeof xSymbol);
+// non primitive data types
+const student = {
+  name: "shefat",
+  age: 24,
+  isPassed: true,
+};
+student["age"] = student["age"] + 1;
+student["name"] = "Shefat";
+console.log(student["age"]);
+const xarray = ["shefat", "Shefat"]
+// also funtion is non primitive data types 
