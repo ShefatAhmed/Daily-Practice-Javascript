bd589c0da8c3867d558ee295863a330ccce9722c
Shefat Ahmed <shefata69@gmail.com>
2024-12-18 15:39:58 +0600

    Variable Rules

--- Variables & Data Types/first.js
+++ Variables & Data Types/first.js
@@ -2,11 +2,24 @@
 // console.log("Welcome , Shefat Ahmed");
 // console.log("How are you!");
 
-fullName = "Shefat Ahmed"
-age=24
-price=101
-radius=44.55
-a=null
-y=undefined
-isFollow=true //or false
-console.log(isFollow)
+// fullName = "Shefat Ahmed"
+// age=24
+// price=101
+// radius=44.55
+// a=null
+// y=undefined
+// isFollow=true //or false
+// console.log(isFollow)
+
+// fullName= "Kandapara"
+// fullname= "Malshapara"
+
+// console.log(fullName)
+
+// fullName = "tony stark";
+// _fullName = "tony stark";
+// $fullName = "tony stark";
+// fullname ="tony stark"
+
+// console= "tony stark" //reserve keyword
+// console.log(console);
