f7003e513e45d3d6fdf1c6418aae9192a3810002
Shefat Ahmed <shefata69@gmail.com>
2024-12-18 15:32:40 +0600

    normal variable

--- Variables & Data Types/first.js
+++ Variables & Data Types/first.js
@@ -1,3 +1,12 @@
-console.log("Hello World!");
-console.log("Welcome , Shefat Ahmed");
-console.log("How are you!");
+// console.log("Hello World!");
+// console.log("Welcome , Shefat Ahmed");
+// console.log("How are you!");
+
+fullName = "Shefat Ahmed"
+age=24
+price=101
+radius=44.55
+a=null
+y=undefined
+isFollow=true //or false
+console.log(isFollow)
