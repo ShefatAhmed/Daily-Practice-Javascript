18fc0a6802ec7ef841074033a97b3e2a228851e2
Shefat Ahmed <shefata69@gmail.com>
2024-12-20 21:27:38 +0600

    loops practice

---
+++ Loops and Strings/script.js
@@ -0,0 +1,60 @@
+// for Loop
+// for (let count = 0; count <= 5; count++) {
+//   console.log("object"); // 5 excute
+// }
+
+// calculate sum of 1 to 5
+// let sum = 0;
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// While loop
+// let i = 0;
+// while (i <= 5) {
+//   console.log("object");
+//   i++;
+// }
+
+// do-while Loop
+// let i = 60;
+// do {
+//   console.log("object");
+//   i++;
+// } while (i <= 5);
+
+// for of loop
+// let str = "object";
+// for (let val of str) {
+//   console.log(val);
+// }
+// let str = "object";
+// let length = 0;
+// for (let val of str) {
+//   console.log(val);
+//   length++;
+// }
+// console.log(length);
+
+// for - in Loop
+// let student = {
+//   name: "Shefat",
+//   age: 24,
+//   isMarried: false,
+//   cgp: 5.0,
+// };
+// for (let key in student) {
+//   console.log("Key: ", key, "Value: ", student[key]);
+// }
+
+// Qs1: Print all even numbers from 0 to 100
+// for (let num = 0; num <= 100; num++) {
+//   if (num % 2 == 0) {
+//     console.log(num);
+//   }
+// }
+
+// QS2: Create a game where you start with any random game number. Ask the user to keep guessing then game number until the user enters correct value.
+// let gameNumber = 25;
