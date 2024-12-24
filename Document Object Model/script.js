11aac437b7100a97cd8c27df0a211bc35d9f450a
Shefat Ahmed <shefata69@gmail.com>
2024-12-24 13:41:11 +0600

    dom practice

---
+++ Document Object Model/script.js
@@ -0,0 +1,28 @@
+// let header = document.getElementById("header");
+// console.dir(header);
+// let header1 = document.getElementsByClassName("header1");
+// console.dir(header1);
+// document.getElementsByTagName("p")
+// let element = document.querySelector("p") // first element
+// let element = document.querySelector(".header") // first element
+// let element1 = document.querySelectorAll("p") // all element
+
+// Attributes
+// let div = document.querySelector("div");
+// let id = div.getAttribute("id");
+// let name = div.getAttribute("name");
+// let para = document.querySelector("p");
+// para.setAttribute("class", "213");
+
+// Style
+// let div = document.querySelector("div");
+// div.style.backgroundColor = "black"
+
+// Insert Elements
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click Me!";
+// let div = document.querySelector("div");
+// div.append(newBtn); // last
+// div.prepend(newBtn); // first
+// div.before(newBtn); // before div
+// div.after(newBtn); // after div
