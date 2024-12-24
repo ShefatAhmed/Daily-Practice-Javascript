1a13aef8b17cd1b6ebb056b9e85c59db0c99ca86
Shefat Ahmed <shefata69@gmail.com>
2024-12-24 15:18:33 +0600

    Events practice

---
+++ Events in JavaScript/script.js
@@ -0,0 +1,34 @@
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () => {
+//   console.log("btn1 was clicked");
+// let a = 25;
+// a++;
+// console.log(a);
+// };
+
+// let div = document.querySelector("div");
+// div.onmouseover = () => {
+//   console.log("you are inside div");
+// };
+
+// btn1.addEventListener("click", ()=> {
+//         console.log("button was clicked");
+// })
+
+// let modeBtn = document.querySelector("#mode");
+// let currMode = "light";
+// let body = document.querySelector("body");
+
+// modeBtn.addEventListener("click", () => {
+//   if (currMode === "light") {
+//     currMode = "dark";
+//     body.classList.add("dark");
+//     body.classList.remove("white");
+//   } else {
+//     currMode = "light";
+//     body.classList.add("white");
+//     body.classList.remove("dark");
+//   }
+//   console.log(currMode);
+// });
