5b215b7bf0173c03a81a2a2a51b23a9697800c30
Shefat Ahmed <shefata69@gmail.com>
2025-01-01 22:19:11 +0600

    solve hackerrank day 1 challenge

---
+++ Classes & Objects/app.js
@@ -0,0 +1,23 @@
+// Classes & Objects
+
+// method
+// const student = {
+//   fullName: "Shefat Ahmed",
+//   marks: 94.5,
+//   printMarks: function () {
+//     console.log("marks =", this.marks);
+//   }, // method
+// };
+
+// prototype
+// const employee = {
+//   calcTax() {
+//     console.log("tax rate is 10%");
+//   },
+// };
+// const karanArjun = {
+//   salary: 50000,
+// };
+// karanArjun.__proto__ = employee;
+
+// class 
---
+++ HackerRank/Day1.js
@@ -0,0 +1,51 @@
+// function getArea(length, width) {
+//         let area;
+//         area = length * width; // ক্ষেত্রফলের সঠিক ফর্মুলা
+
+//         return area;
+//       }
+// function getArea(length, width) {
+//   let area;
+//   area = (length * 2) + (width * 2);
+
+//   return area;
+// }
+// console.log(getArea(5,10));
+
+// function vowelsAndConsonants(s) {
+//   let consonants = [];
+//   for (let val of s) {
+//     if (
+//       val === "a" ||
+//       val === "e" ||
+//       val === "i" ||
+//       val === "o" ||
+//       val === "u"
+//     ) {
+//       console.log(val);
+//     } else if (val !== " ") {
+//       consonants.push(val);
+//     }
+//   }
+//   for (let consonant of consonants) {
+//     console.log(consonant);
+//   }
+// }
+// vowelsAndConsonants("shefat")
+
+// function regexVar() {
+//   const re = /^([aeiou]).*\1$/;
+//   return re;
+// }
+
+// function reverseString(s) {
+//   try {
+//     s = s.split("").reverse().join("");
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     console.log(s);
+//   }
+// }
+
+// reverseString("shefat");
